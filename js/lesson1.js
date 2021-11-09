c4d155912bdc050ae9b72588ef51d52aa39b9c45
Mirko40 <mmirko@ua.fm>
2021-11-09 21:25:47 +0200

    Init

--- js/lesson1.js
+++ js/lesson1.js
@@ -1,28 +1,36 @@
 "use strict"
-function whatName() {
+function getName() {
     let yourName = prompt("What is your name?");
-    alert("Hello" + " " + yourName)
+    alert(`Hello ${yourName}`)
 };
-function whatAge() {
+function getAge() {
     const YEAR = 2021;
-    let yearBirth = +prompt("Year jf birth?");
-    alert("You are" + " " + (YEAR - yearBirth) + " " + "years")
+    let yearBirth = +prompt("Year of your birth?");
+    alert(`You are ${YEAR - yearBirth} years`)
 };
 
-function whatPerimetr() {
+function getPerimetr() {
     let side = +prompt("What is a side of a aquqre (cm)?");
-    alert("P=" + (side * 4) + "cm")
+    alert(`P= ${(side * 4)} cm`)
 };
 
-function whatSquare() {
+function getSquare() {
     let circleR = +prompt("What is a radius of a circle (cm)?");
-    alert("S=" + (Math.PI * circleR * circleR) + "cm2")
+    alert(`S=${Math.PI * circleR * circleR} cm2`)
 };
 
-function whatSpeed() {
+function getSpeed() {
     let lenght = +prompt("What is a lenght (km)?");
-    let time = +prompt("What is a time for travel (hours)?");
-    alert("V=" + (lenght / time) + "km/h")
+    let time = +prompt("What is a time for travel (in hours)?");
+    alert(`V= ${lenght / time} km/h`)
+};
+function exchDollar() {
+    const CURS = 0.86;
+    let howDollars = +prompt("How many dollars to convert into euros?");
+    alert(`You have ${CURS * howDollars} euros`)
 };
 
-
+function getFiles() {
+    let volumeF = +prompt("What is the size of the flash drive in GB?");
+    alert(`You can copy ${((volumeF * 1024) / 820).toFixed(0)} files`)
+};
